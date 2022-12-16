5e53c89e7ffc77ba2e98ece64fde494527410ef5
Romina Chazarreta <rchazarreta@gmail.com>
2022-12-16 19:21:51 -0300

    Agrego el segundo script y la validación del label del botón VACIAR CARRITO

--- script_pbacompleta2.py
+++ script_pbacompleta2.py
@@ -61,3 +61,32 @@ def check_label(s):
 check_label(labelBotonAgregar)
 print(f"El label que tiene el botón es: {labelBotonAgregar}")
 print("---------------------------------------------------------------------------------------------")
+# Segundo script y validación del label del botón "VACIAR CARRITO"
+time.sleep(2)
+driver.find_element(By.XPATH, '//*[@id="downshift-0-input"]').send_keys("bata de baño infantil con capucha")
+driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div[1]/div/div/div/div[2]/div/div[1]/div/div[3]/div/div/div[1]/div/div/div[1]/div/label/div/span/div/div/button").click()
+time.sleep(15)
+# driver.find_element(By.XPATH, "/html/body/div[4]/div/div/div/div[2]/a[2]").click()
+driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div[2]/div/div/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[2]/div/section/a/article/div[2]/div[1]/div/div/img[1]").click()
+time.sleep(15)
+driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/div[2]/div/div[1]/div[2]/section/div/div[2]/div/div[5]/div/div/div[1]/div/div/div/div/div/div/div[1]/button").click()
+driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/div[2]/div/div[1]/div[2]/section/div/div[2]/div/div[5]/div/div/div[1]/div/div/div/div/div/div/div[1]/button").click()
+time.sleep(1)
+driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/div[2]/div/div[1]/div[2]/section/div/div[2]/div/div[5]/div/div/div[2]/div/div/button").click()
+time.sleep(5)
+driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/div[1]/div/div/div/div[2]/div/div[1]/div/div[3]/div/div/div[3]/aside/div/div/button").click()
+time.sleep(7)
+
+# Validación label boton "VACIAR CARRITO".
+botonVaciarCarrito = driver.find_element(By.XPATH, "/html/body/div[6]/div/div[2]/div/div/div[3]/div/div/button")
+labelVaciarCarrito = botonVaciarCarrito.text
+
+labelRequerido = "VACIAR CARRITO"
+
+print(f"El label requerido del botón es: {labelRequerido}")
+
+check_label(labelVaciarCarrito)
+print(f"El label que tiene el botón es: {labelVaciarCarrito}")
+print("------------------------------------------------------------------------------------------")
+
+driver.find_element(By.XPATH, "/html/body/div[6]/div/div[1]/button").click()
